424b976aed93ec5ba150ce63d6b33ddc9dbdc53b
SparkFlowGt <scinteiestefan@protonmail.com>
2024-06-23 16:10:22 +0300

    star_ratingComp

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
